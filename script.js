fb8e36336434d7cb53bce735074f365a000eb3a4
Kemal Yavuz İmer <yavuz.imer133@gmail.com>
2022-05-04 15:09:01 +0300

    question-answer-game

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const start = () => {
     if (selectID.style.display === "none") {
         selectID.style.display = "block";
     }
-    document.querySelector(".time-table").innerHTML = count
     let query = api
     fetch(query).then(question => {
         return question.json()
@@ -20,16 +19,48 @@ const displayResult = (result) => {
 const btnClick = (e) => {
     console.log(e.target.id);
     let _index = index;
-    isControl(_index, e.target.id)
+    console.log("btnClick");
+    let time = 0
+    let intervalID1 = setInterval(stop = () => {
+        if (time < 3) {
+            document.querySelector(`#${e.target.id}`).style.backgroundColor = "orange"
+            time++
+            console.log("time 1 : " + time);
+        }
+        else {
+            console.log("gel 1");
+            clearInterval(intervalID1)
+            isControl(_index, e.target.id)
+        }
+    }, 1000)
 }
 
 let _true = 0, _false = 0
 const isControl = (i, answer) => {
-    if (arr[i].Answer === answer) _true++
-    else _false++
-    document.querySelector(".true").innerHTML = _true
-    document.querySelector(".false").innerHTML = _false
-    main()
+    console.log("isControl");
+    if (arr[i].Answer === answer) {
+        _true++
+        document.querySelector(`#${answer}`).style.backgroundColor = "green"
+    }
+    else {
+        _false++
+        document.querySelector(`#${answer}`).style.backgroundColor = "red"
+    }
+    document.querySelector(".true").innerHTML = `${_true} Doğru`
+    document.querySelector(".false").innerHTML =`${_false} Yanlış`
+    let time = 0
+    let intervalID = setInterval(stop = () => {
+        if (time < 1) {
+            time++
+            console.log("time 2 : " + time);
+        } else {
+            console.log("gel 2");
+            clearInterval(intervalID)
+            main()
+        }
+    }, 1000)
+    if (time === 2) {
+    }
 }
 
 const clickOn = () => {
@@ -52,9 +83,14 @@ function main() {
 }
 
 const queryGet = () => {
+    document.querySelector(".question-count").innerHTML =`${ index + 1}. Soru`
     document.querySelector(".question").innerHTML = arr[index].question
     document.querySelector("#A").innerHTML = arr[index].A
     document.querySelector("#B").innerHTML = arr[index].B
     document.querySelector("#C").innerHTML = arr[index].C
     document.querySelector("#D").innerHTML = arr[index].D
+    let a = ["A", "B", "C", "D"]
+    a.forEach(e => {
+        document.getElementById(`${e}`).style.backgroundColor = "#008080"
+    });
 }
